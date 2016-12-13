784abf5d9a2daee0dfa71a34c476a4d52ee173df
Jeff Bentler <bentljm@gmail.com>
2016-12-12 19:21:31 -0800

    Completed review of getElementsByClassName and stringifyJSON. Also updated FILL-ME-IN in spec docs

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,19 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className) {
   // your code here
-};
+  var elements = [];
+  var testElement = function(element) {
+    if(element.classList) {
+      if(element.classList.contains(className)) {
+        elements.push(element);
+      }
+    }
+    for(var i = 0; i < element.childNodes.length; i++) {
+      testElement(element.childNodes[i]);
+    }
+  }
+  testElement(document.body);
+  return elements;
+}
--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,5 +4,36 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+//Number, boolean, function, string, array, obj
+  if(typeof obj === 'number' || typeof obj === 'boolean' || typeof obj === 'function') {
+    return obj.toString();
+  } else if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return 'undefined';
+  } else if(typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if(Array.isArray(obj)) {
+    var result = [];
+    for(var i = 0; i < obj.length; i++) {
+      result.push(stringifyJSON(obj[i]));
+    }
+    return '[' + result.join(',') + ']';
+  } else if(typeof obj === 'object') {
+    var keys = Object.keys(obj);
+    if(keys.length) {
+      var values = new Array();
+      for(var i = 0; i < keys.length; i++) {
+        if(obj[keys[i]] === undefined || typeof obj[keys[i]] === 'function') {
+          continue;
+        }
+        var keyString = '"' + keys[i] + '":';
+        var indexValue = stringifyJSON(obj[keys[i]]);
+        values.push(keyString + indexValue);
+      }
+      return '{' + values.join(",") + '}';
+    } else {
+      return '{}';
+    }
+  }
 };
